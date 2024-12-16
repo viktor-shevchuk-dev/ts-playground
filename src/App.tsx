aebdcb372a899dbb40b76aac357c4ac9253c78b5
Viktor Shevchuk <viktor.shevchuk.dev@gmail.com>
2024-12-16 20:57:10 +0100

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import AddTodoForm from './components/AddTodoForm';
 import TodoItem from './components/TodoItem';
 
 import './App.css';
+
+import { addTodo, deleteTodo } from './todoSlice';
+import { RootState } from './store';
 
 export interface Task {
   id: number;
@@ -11,24 +15,30 @@ export interface Task {
 }
 
 function App() {
-  const [todos, setTodos] = useState<Task[]>([]);
+  // const [todos,  setTodos] = useState<Task[]>([]);
+  const todos = useSelector((state: RootState) => state.todos);
+  const dispatch = useDispatch();
 
-  const addTodo = (text: string) => {
-    const newTodo = { id: Date.now(), text };
-    setTodos((todos) => [...todos, newTodo]);
-  };
+  // const addTodo = (text: string) => {
+  //   const newTodo = { id: Date.now(), text };
+  //   setTodos((todos) => [...todos, newTodo]);
+  // };
 
-  const deleteTodo = (id: number) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
-  };
+  // const deleteTodo = (id: number) => {
+  //   setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  // };
 
   return (
     <>
       <h1>Todo List</h1>
-      <AddTodoForm onAdd={addTodo} />
+      <AddTodoForm onAdd={(text) => dispatch(addTodo(text))} />
       <ul>
         {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} onDelete={deleteTodo} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onDelete={(id) => dispatch(deleteTodo(id))}
+          />
         ))}
       </ul>
     </>
---
+++ src/todoSlice.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface Task {
+  id: number;
+  text: string;
+}
+
+const initialState: Task[] = [];
+
+const todoSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.push({
+        id: Date.now(),
+        text: action.payload,
+      });
+    },
+    deleteTodo(state, action: PayloadAction<number>) {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todoSlice.actions;
+export default todoSlice.reducer;
